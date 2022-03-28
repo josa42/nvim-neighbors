f7cae2727494770e115ef24ecddefcd8d2ae3a77
Josa Gesell <josa@gesell.me>
2022-03-28 19:01:09 +0200

    chore: cleanup command creation

--- lua/jg/neighbors.lua
+++ lua/jg/neighbors.lua
@@ -1,21 +1,36 @@
 local M = {}
+local l = {}
 
 function M.setup()
-  vim.cmd("command! Ejs   lua require('jg.neighbors').open('js')")
-  vim.cmd("command! Etest lua require('jg.neighbors').open('test.js')")
-  vim.cmd("command! Ecss  lua require('jg.neighbors').open('css')")
-  vim.cmd("command! Tjs   lua require('jg.neighbors').open('js',      'tabedit')")
-  vim.cmd("command! Ttest lua require('jg.neighbors').open('test.js', 'tabedit')")
-  vim.cmd("command! Tcss  lua require('jg.neighbors').open('css',     'tabedit')")
-  vim.cmd("command! Vjs   lua require('jg.neighbors').open('js',      'vsplit')")
-  vim.cmd("command! Vtest lua require('jg.neighbors').open('test.js', 'vsplit')")
-  vim.cmd("command! Vcss  lua require('jg.neighbors').open('css',     'vsplit')")
+  local cmds = {
+    E = 'edit',
+    T = 'tabedit',
+    V = 'vsplit',
+  }
+
+  local actions = {
+    test = 'test.js',
+    js = 'js',
+    css = 'css',
+  }
+
+  for key, cmd in pairs(cmds) do
+    for action, ext in pairs(actions) do
+      vim.api.nvim_add_user_command(key .. action, l.cmd(ext, cmd), {})
+    end
+  end
 end
 
 function M.open(ext, cmd)
   cmd = cmd or 'edit'
   local fname = vim.fn.substitute(vim.fn.expand('%:r'), '\\v\\.tests?', '', '')
   vim.cmd(cmd .. ' ' .. fname .. '.' .. ext)
+end
+
+function l.cmd(ext, cmd)
+  return function()
+    M.open(ext, cmd)
+  end
 end
 
 return M
